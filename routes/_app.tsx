630fe924c5408e49e6c699ccc9a7639c8b0bb3c6
Shajan Jacob <shajanjp@gmail.com>
2023-09-24 20:37:51 +0530

    fix: title and header

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component }: AppProps) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>piravom.in</title>
+        <title>Piravom</title>
       </head>
       <body>
         <Component />
