ef263f050549ed103578f78c1a1e04a265148e3f
Shajan Jacob <shajanjp@gmail.com>
2023-10-03 08:27:52 +0530

    feat: updates landing page

--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,15 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  theme: {
+    extend: {
+      colors: {
+        brandBlue: '#0AB',
+        brandGray: '#222', 
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans'],
+      },
+    },
+  },
 } as Options;
