6356c81dfb1494141a1b43f8d6d7e8bfadca2610
Shajan Jacob <shajanjp@gmail.com>
2023-10-13 21:53:35 +0530

    feat: background updates

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,10 +4,10 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <body class="flex items-center justify-center h-screen bg-cover bg-center bg-no-repeat" style="background-image: url('https://i.imgur.com/HrTavfe.jpg')">
+    <body class="flex items-center justify-center h-screen bg-green-600">
     <div class="text-center">
-    <h1 class="text-4xl font-bold text-white font-roboto font-normal" style="text-shadow: 0 5px 10px black;">piravom.in</h1>
-    <p class="mt-4 text-lg font-roboto font-light text-white" style="text-shadow: 0 5px 10px black;">Coming soon...</p>
+    <h1 class="text-4xl font-bold text-white font-roboto text-5xl">www.piravom.in</h1>
+    <p class="mt-4 text-lg font-roboto font-light text-white">Coming soon...</p>
     </div>
     </body>
     );
