0df4921dc9fd6904be1f51a043c3a9489bdbebfa
Shajan Jacob <shajanjp@gmail.com>
2025-08-20 09:06:49 +0530

    fix: update footer to include links for authors

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer class="text-center mt-10 pt-5 pb-5 bg-slate-50">
-      Made with ❤️ by the Shajan & Emil
+      Made with ❤️ by <a href="https://www.instagram.com/shajanjp/" target="_blank" rel="noopener noreferrer" className="text-blue-400">Shajan</a> & <a href="https://www.instagram.com/emilrosemathew/" target="_blank" rel="noopener noreferrer" className="text-pink-400" >Emil</a>
     </footer>
   );
 }
